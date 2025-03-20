de9a212e5b8adaad5fe61baaae1aa5194b158a1f
Erick Cocoletzi <erickalbertosantillancocoletzi@gmail.com>
2025-03-19 18:23:51 -0600

    nuevo cambio

--- untitled/src/paquetedeprueba/ejercicio1.java
+++ untitled/src/paquetedeprueba/ejercicio1.java
@@ -9,5 +9,6 @@ public class ejercicio1 {
         }
         System.out.println("que tal amigos");
         System.out.println("buenas noches perros");
+        System.out.println("sabes");
     }
 }
